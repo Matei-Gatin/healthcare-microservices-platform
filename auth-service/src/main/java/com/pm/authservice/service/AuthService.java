a2a107f68881155f37c3126173d83fddf458163c
Matei-Gatin <thedackchair@gmail.com>
2025-08-14 10:45:19 +0200

    Add initial implementation of patient management services and related configurations

---
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -0,0 +1,45 @@
+package com.pm.authservice.service;
+
+import com.pm.authservice.dto.LoginRequestDto;
+import com.pm.authservice.dto.LoginResponseDto;
+import com.pm.authservice.model.User;
+import com.pm.authservice.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+    private static final Logger log = LoggerFactory.getLogger(AuthService.class);
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public AuthService(UserService userService, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+    }
+
+    public Optional<String> authenticate(LoginRequestDto loginRequestDto) {
+        Optional<String> token = userService.findByEmail(loginRequestDto.getEmail())
+                .filter(u -> passwordEncoder.matches(loginRequestDto.getPassword(), u.getPassword()))
+                .map(u -> jwtUtil.generateToken(u.getEmail(), u.getRole()));
+
+        return token;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            jwtUtil.validateToken(token);
+            return true;
+        } catch (JwtException e) {
+            log.error(e.toString());
+            return false;
+        }
+    }
+}
---
+++ auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
@@ -0,0 +1,56 @@
+package com.pm.authservice.util;
+
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import org.hibernate.annotations.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.security.KeyPair;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final Key secretKey;
+
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
+        byte[] keyBytes = Base64.getDecoder().decode(secret.getBytes(
+                StandardCharsets.UTF_8
+        ));
+        this.secretKey = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(String email, String role) {
+        return Jwts
+                .builder()
+                // PAYLOAD:
+                .subject(email)
+                .claim("role", role)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 ^ 60 * 10)) // 10 hours
+                //
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public void validateToken(String token) {
+        try {
+            Jwts.parser().verifyWith((SecretKey) secretKey)
+                    .build()
+                    .parseSignedClaims(token);
+        } catch (SignatureException e) {
+            throw new JwtException("Invalid JWT signature");
+        } catch (JwtException e) {
+            throw new JwtException("Invalid Jwt");
+        }
+    }
+}
